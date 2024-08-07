8f14727d65b247eb90c4b9bf84dab6f5a84f4c86
hong138 <hong_kwok16@hotmail.com>
2024-08-07 17:29:07 +0800

    2024.08.07

--- questions/src/JavaQuest2.java
+++ questions/src/JavaQuest2.java
@@ -8,16 +8,16 @@ public class JavaQuest2 {
       System.out.println("a is between 0 and 8");
     } else if (a > 9){
       System.out.println("a > 9");
-    }else {
+    } else {
       System.out.println("a < 0");
     }
 
     String str = "I love Java. I love coding";
     if (str.length() == 27) {
       System.out.println("str length=27");
-    } else if (str.length() < 27) { // code here ... add the condition
+    } else if (str.length() < 27) { 
       System.out.println("str length < 27");
-    } else if (str.length() > 27) { // code here ... complete the code
+    } else if (str.length() > 27) { 
       System.out.println("str length > 27");
     }
   }
