284f92973c67d7d619919af91146d15d6cd8d24d
hong138 <hong_kwok16@hotmail.com>
2024-08-14 09:44:26 +0800

    update

--- questions/src/JavaQuest11.java
+++ questions/src/JavaQuest11.java
@@ -3,6 +3,12 @@ public class JavaQuest11 {
     int delayedArrivalTime1 = findDelayedArrivalTime(15, 5); // 20
     int delayedArrivalTime2 = findDelayedArrivalTime(23, 5); // 4
     int delayedArrivalTime3 = findDelayedArrivalTime(18, 6); // 0
+    int delayedArrivalTime4 = findDelayedArrivalTime(9, 6);
+
+    System.out.println("Delayed Arrival Time 1: " + delayedArrivalTime1);
+    System.out.println("Delayed Arrival Time 2: " + delayedArrivalTime2);
+    System.out.println("Delayed Arrival Time 3: " + delayedArrivalTime3);
+    System.out.println("Delayed Arrival Time 4: " + delayedArrivalTime4);
   }
 
   public static int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
